61f958cc1515f0a383fa3730f9b71a80d91991e4
kritika <71543966+kritikakaura1518@users.noreply.github.com>
2021-05-29 15:14:45 +0530

    Update conn.java

--- source/conn.java
+++ source/conn.java
@@ -1,1 +1,19 @@
+package Empolyee;
+import java.sql.*;
+
+public class conn{
+    
+    public Connection c;
+    public Statement s;
+ 
+    public conn(){
+        try{
+        	Class.forName("com.mysql.cj.jdbc.Driver");
+			c=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","KRITIKAKAURA"," ");
+            s = c.createStatement();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
 
